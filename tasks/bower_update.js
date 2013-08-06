f707872a48312b2c9da21bd4faf204d5824bbe54
Johannes Stein <johannesstein@freeze-dev.com>
2013-08-06 21:24:00 +0200

    Added bower update command

---
+++ tasks/bower_update.js
@@ -0,0 +1,40 @@
+/*
+ * grunt-bower-commands
+ * https://github.com/Stoney-FD/grunt-bower-commands
+ *
+ * Copyright (c) 2013 Johannes Stein
+ * Licensed under the MIT license.
+ */'use strict';
+
+var bower = require('bower');
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('bower_update', 'Update bower packages', function() {
+    var done = this.async();
+
+    var options = this.options({
+      config: {},
+      done: null,
+      opts: {},
+      names: []
+    });
+
+    var smallOptions = {
+      paths: options.paths,
+      offline: options.offline
+    };
+
+    bower.commands.update(options.names, options.opts, options.config).on('end', function() {
+      if ( typeof options.done === 'function') {
+        options.done.apply(this, arguments);
+      }
+
+      done();
+    });
+  });
+
+};
